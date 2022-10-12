10343129cbb5caf4f2d5fdf7068642ef08b5b0b6
siam-26 <siamshahriar390@gmail.com>
2022-10-12 21:04:08 +0600

    added tostify and show correct answers

--- src/Component/ShowOptions/ShowOptions.js
+++ src/Component/ShowOptions/ShowOptions.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './ShowOptions.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-const ShowOptions = ({ option }) => {
+const ShowOptions = ({ option, questions }) => {
+    const choseAnswerHandler = () => {
+        if (option === questions) {
+            const notify = () => toast('Correct Answer!!!');
+            return notify();
+        }
+        else {
+            const notify = () => toast('Wrong Answer!!!');
+            return notify();
+        }
+    }
 
     return (
         <div className='showoptions'>
-            <p>{option}</p>
+            <p onClick={choseAnswerHandler}>{option}</p>
+            <ToastContainer></ToastContainer>
         </div>
     );
 };
--- src/Component/StartQuiz/QuestionsStartQuiz/QuestionsStartQuiz.js
+++ src/Component/StartQuiz/QuestionsStartQuiz/QuestionsStartQuiz.js
@@ -2,20 +2,33 @@ import React from 'react';
 import ShowOptions from '../../ShowOptions/ShowOptions';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './QuestionsStartQuiz.css';
-import { faCoffee, faEye } from '@fortawesome/free-solid-svg-icons';
+import { faEye } from '@fortawesome/free-solid-svg-icons';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const QuestionsStartQuiz = ({ questions }) => {
     const { question } = questions;
+    const correctAnswer = questions.correctAnswer;
+
+    const correctAnswerHandler = () => {
+        const notify = () => toast(correctAnswer);
+        return notify();
+    }
 
     return (
         <div className='questions'>
             <div className='question-child-div'>
-                <h5><b>Ques:</b> {question} </h5><FontAwesomeIcon className='eyeIcon' icon={faEye}></FontAwesomeIcon>
+                <h5><b>Ques:</b> {question} </h5>
+
+                <FontAwesomeIcon onClick={correctAnswerHandler} className='eyeIcon' icon={faEye}></FontAwesomeIcon>
+
+                <ToastContainer></ToastContainer>
             </div>
             {
                 questions.options.map(option => <ShowOptions
                     key={option}
                     option={option}
+                    questions={questions.correctAnswer}
                 ></ShowOptions>)
             }
         </div>
